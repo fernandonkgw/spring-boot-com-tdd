3e578154134a3357757ea03ee4554c0c76e002be
Fernando Nakagawa <fernandonkgw@gmail.com>
2020-03-15 11:08:56 -0300

    classes modelo

---
+++ src/main/java/com/example/demo/modelo/Telefone.java
@@ -0,0 +1,32 @@
+package com.example.demo.modelo;
+
+public class Telefone {
+
+    private String ddd;
+    private String numero;
+    private Pessoa pessoa;
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Pessoa getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+}
