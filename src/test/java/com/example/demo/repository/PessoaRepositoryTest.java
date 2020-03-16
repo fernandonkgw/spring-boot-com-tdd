85130dcd8313bfc756eb5e19f1429da8d80ecb15
Fernando Nakagawa <fernandonkgw@gmail.com>
2020-03-16 10:49:42 -0300

    integration test da busca por ddd e telefone no repository

--- src/test/java/com/example/demo/repository/PessoaRepositoryTest.java
+++ src/test/java/com/example/demo/repository/PessoaRepositoryTest.java
@@ -41,4 +41,22 @@ public class PessoaRepositoryTest {
 
         assertThat(optional.isPresent()).isFalse();
     }
+
+    @Test
+    public void deve_encontrar_pessoa_pelo_ddd_e_numero_de_telefone() {
+        final Optional<Pessoa> optional = sut.findByTelefoneDddAndTelefoneNumero("82", "39945903");
+
+        assertThat(optional.isPresent()).isTrue();
+        final Pessoa pessoa = optional.get();
+        assertThat(pessoa.getCodigo()).isEqualTo(2L);
+        assertThat(pessoa.getNome()).isEqualTo("Pedro");
+        assertThat(pessoa.getCpf()).isEqualTo("55565893569");
+    }
+
+    @Test
+    public void nao_deve_encontrar_pessoa_cujo_ddd_e_telefone_nao_estejam_cadastrados() {
+        final Optional<Pessoa> optional = sut.findByTelefoneDddAndTelefoneNumero("23", "3454354353");
+
+        assertThat(optional.isPresent()).isFalse();
+    }
 }
